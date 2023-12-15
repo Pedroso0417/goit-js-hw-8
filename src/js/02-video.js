3fc99ca89bdee2d487c76eeee5d2b69c8fc67442
Pedroso0417/goit-js-hw-8 <136574676+Pedroso0417@users.noreply.github.com>
2023-12-16 07:46:07 +0800

    Commit Message

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,94 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+// Define handleThrottle before using it in player.on()
+const handleThrottle = function (data) {
+  const timeInSeconds = data.seconds;
+  console.log(timeInSeconds);
+  localStorage.setItem('videoplayer-current-time', timeInSeconds.toString());
+};
+
+player.on('timeupdate', throttle(handleThrottle, 100));
+
+// Use parseFloat when setting the current time
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player
+    .setCurrentTime(parseFloat(savedTime))
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
+
+// import Vimeo from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+// // Get the Vimeo player element by its ID
+// const iframe = document.getElementById('vimeo-player');
+
+// // Initialize the Vimeo player
+// const player = new Vimeo(iframe);
+
+// // Add an event listener for timeupdate
+// player.on('timeupdate', data => {
+//   const currentTime = data.seconds;
+
+//   // Save the playback time to local storage
+//   localStorage.setItem('videoplayer-current-time', currentTime);
+// });
+
+// // Get the saved playback time from local storage
+// const savedTime = localStorage.getItem('videoplayer-current-time');
+
+// // Check if there is a saved time and set it
+// if (savedTime) {
+//   player.setCurrentTime(parseFloat(savedTime));
+// }
+
+// // Throttle the function to update storage once a second
+// const savePlaybackTime = throttle(time => {
+//   localStorage.setItem('videoplayer-current-time', time);
+// }, 1000);
+
+// // Add an event listener for timeupdate with throttled function
+// player.on('timeupdate', data => {
+//   const currentTime = data.seconds;
+//   console.log(currentTime); // Corrected variable name
+
+//   // Save the playback time to local storage using throttled function
+//   savePlaybackTime(currentTime);
+// });
+
+// import VimeoPlayer from 'vimeo-player';
+// import throttle from 'lodash.throttle';
+
+// const player = new VimeoPlayer('myPlayer', {
+//   // Options here if needed
+// });
+
+// player.on(
+//   'timeupdate',
+//   throttle(data => {
+//     const currentTime = data.seconds; // Current playback time in seconds
+//     localStorage.setItem('videoplayer-current-time', currentTime.toString());
+//   }, 1000)
+// ); // Throttle to at most once per second
+
+// // Resume playback from the saved position
+// const savedTime = localStorage.getItem('videoplayer-current-time');
+// if (savedTime) {
+//   player.setCurrentTime(parseFloat(savedTime));
+// }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,53 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Throttle the function to run at most once every 500 milliseconds
+  const throttleUpdateStorage = _.throttle(updateStorage, 500);
 
+  const form = document.querySelector('.feedback-form');
+
+  // Track input event on the form
+  form.addEventListener('input', function () {
+    throttleUpdateStorage();
+  });
+
+  // Load stored data on page load
+  loadStoredData();
+
+  // Submit form
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    submitForm();
+  });
+});
+
+function updateStorage() {
+  const formData = {
+    email: document.querySelector('input[name="email"]').value,
+    message: document.querySelector('textarea[name="message"]').value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function loadStoredData() {
+  const storedData = localStorage.getItem('feedback-form-state');
+
+  if (storedData) {
+    const formData = JSON.parse(storedData);
+    document.querySelector('input[name="email"]').value = formData.email;
+    document.querySelector('textarea[name="message"]').value = formData.message;
+  }
+}
+
+function submitForm() {
+  // Display form data in the console
+  const formData = {
+    email: document.querySelector('input[name="email"]').value,
+    message: document.querySelector('textarea[name="message"]').value,
+  };
+  console.log('Form data submitted:', formData);
+
+  // Clear storage and form fields
+  localStorage.removeItem('feedback-form-state');
+  document.querySelector('input[name="email"]').value = '';
+  document.querySelector('textarea[name="message"]').value = '';
+}
