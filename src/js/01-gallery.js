e1b36d088107129e6c71a2f7da5ea86431c78b4c
Pedroso0417/goit-js-hw-6 <136574676+Pedroso0417@users.noreply.github.com>
2023-12-16 02:57:15 +0800

    commit-1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import { galleryItems } from './gallery-items.js';
+
+// Change code below this line
+
+const galleryList = document.querySelector('.gallery');
+
+const createGallery = elements => {
+  return elements
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}"/>
+          </a>
+        </li>
+      `;
+    })
+    .join('');
+};
+
+const photosMarkup = createGallery(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', photosMarkup);
+
+// Second task //
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
+
+lightbox.on('show.simplelightbox', function (e) {
+  console.log('Image shown', e);
+});
